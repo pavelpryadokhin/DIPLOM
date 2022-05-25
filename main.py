0d27c7e72fd52a63c43ee45f9e9cc9d5a5cbd02d
Павел <mrpavel23ooo@mail.ru>
2022-05-25 15:44:38 +0300

    Алгоритм динамического ценообразования

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ N=2
 x=np.array([0,0.5,1.0,1.5])
 u={-1,-0.5,0,0.5,1}
 x_cost=x**2
-result=dict()
+result=dict()#TODO
 while N!=0:
     u_effort=np.zeros((len(x),len(x)))
     for i in range(len(x)):
